8938b752197fef36e28d3138b59dabb742e50825
Asana Korede <asanakorededaniel@gmail.com>
2023-11-21 08:48:58 -0800

    add the conversions for both degrees in the script

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+
+  function convertCtoF(celsius) {
+    let fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit;
+  }
+
+  function convertFtoC(fahrenheit) {
+    let celsius = fahrenheit - (32 * 5 / 9)
+    return celsius;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
